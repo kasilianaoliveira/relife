19e8654d85e6085e7400b170b39c13a908c57cfd
kasilianaoliveira <kasilianaoliveira@gmail.com>
2022-11-26 08:01:31 -0300

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getUserLocalStorage } from "../context/util";
 
 export const api = axios.create({
-	baseURL: " https://relife-web-service.onrender.com/"
+	baseURL: " https://relife-web-service.onrender.com"
 
 });
 
